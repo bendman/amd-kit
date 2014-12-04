6f98910670f4b863837135216a65ae8d4a3f9a4a
bendman <duncan.ben@gmail.com>
2014-12-03 23:12:25 -0500

    adding license and updating AMD-Kit

--- amd-kit.js
+++ amd-kit.js
@@ -1,164 +1,272 @@
-/**
- * Create the AMD-Kit Handler
- */
+// 
+// # AMD-Kit
+// 
+// The MIT License (MIT)
+// Copyright (c) 2012 Real Magnet, LLC
+// 
 (function(window, document){
-	var buildFile,
-		Modules = new ModulesList();
+    // Array.indexOf Polyfill (courtesy of MDN)
+    if(!Array.prototype.indexOf){Array.prototype.indexOf=function(a){if(this===null){throw new TypeError}var b=new Object(this);var c=b.length>>>0;if(c===0){return-1}var d=0;if(arguments.length>0){d=Number(arguments[1]);if(d!=d){d=0}else if(d!==0&&d!=Infinity&&d!=-Infinity){d=(d>0||-1)*Math.floor(Math.abs(d))}}if(d>=c){return-1}var e=d>=0?d:Math.max(c-Math.abs(d),0);for(;e<c;e++){if(e in b&&b[e]===a){return e}}return-1}}
 
-	// Array.indexOf Polyfill (courtesy of MDN)
-	if(!Array.prototype.indexOf){Array.prototype.indexOf=function(a){if(this===null){throw new TypeError}var b=new Object(this);var c=b.length>>>0;if(c===0){return-1}var d=0;if(arguments.length>0){d=Number(arguments[1]);if(d!=d){d=0}else if(d!==0&&d!=Infinity&&d!=-Infinity){d=(d>0||-1)*Math.floor(Math.abs(d))}}if(d>=c){return-1}var e=d>=0?d:Math.max(c-Math.abs(d),0);for(;e<c;e++){if(e in b&&b[e]===a){return e}}return-1}}
+    // ## Public Methods
+    var Modules = {
+        // Set AMD configs (a require-like shim config).
+        config: configModuleHandler,
+        // Define a new module.
+        define: handleModule,
+        // Require modules inline.
+        require: function(deps, callback) {
+            handleModule(undefined, deps, callback);
+        },
+        // Manually trigger a module as resolved.
+        execute: function(module) {
+            attemptExecute(module, true);
+        },
+        add: handleModule
+    };
 
-/**
- * ModulesList (Constructor)
- *
- * Basically where the AMD magic happens
- */
-	function ModulesList() {
-		this._modules = {}; // module: [requirements]
-		this._reqs = {}; // requirement: [modules]
-		this._requires = 0;
-	}
-	ModulesList.prototype.add = function(module, reqs, callback) {
-		var i,
-			self = this;
+    // Add a backdoor through require to access other methods.
+    Modules.require.methods = Modules;
 
-		if (typeof reqs === 'string') reqs = [reqs];
-		if (!(reqs instanceof Array)) reqs = [];
-		if (module in this._modules && 'executed' in this._modules[module])
-			return false;
+    // Make primary methods public.
+    window.define = Modules.define;
+    window.require = Modules.require;
 
-		if (module === undefined) {
-			module = 'Require_' + this._requires++;
-		}
 
-		this._modules[module] = {
-			'executed': false,
-			'value': callback,
-			'requirements': reqs
-		};
-		this._reqs[module] = this._reqs[module] || [];
+    // ## Storage variables
+    var modules = {}; // track modules
+    var allDeps = {}; // track dependencies
+    var anonymousCount = 0; // counter for anonymous module IDs
+    var config = {}; // store entire config, for extensibility
+    var shim = {}; // shim dependencies and exports
+    var paths = {}; // proxy paths/nicknames
 
-		i = reqs.length;
-		while (i--) {
-			this._reqs[reqs[i]] = this._reqs[reqs[i]] || [];
-			this.include(reqs[i], executor(reqs[i]));
-			if (this._reqs[reqs[i]].indexOf(module) === -1)
-				this._reqs[reqs[i]].push(module);
-		}
+// 
+// ## handleModule
+// 
+// The base functionality starts here.  This is where incoming modules
+// go for registration and dependency checks.
+    function handleModule(module, deps, callback, isShim) {
+        var i;
 
-		if (typeof callback === 'function') this.execute(module);
+        module = attachType(module);
 
-		function executor(module) {
-			return function() {self.execute(module);};
-		}
-	};
-	ModulesList.prototype.getRequirements = function(module) {
-		var results = [],
-			mods = this._modules,
-			reqs = mods[module].requirements || [],
-			i = reqs.length;
+        if (typeof deps === 'string') deps = [deps];
+        if (!(deps instanceof Array)) deps = [];
+        if (module in modules && 'resolved' in modules[module])
+            return false;
 
-		while (i--) {
-			if (reqs[i] in mods && mods[reqs[i]].executed !== true) {
-				return false;
-			} else if (reqs[i] in mods) {
-				results.unshift(mods[reqs[i]].value);
-			}
-		}
-		return results;
-	};
-	ModulesList.prototype.execute = function(module) {
-		var results,
-			i = this._reqs[module].length,
-			storedModule = this._modules[module],
-			reqs = this.getRequirements(module);
+        if (module === undefined) {
+            module = '_Anonymous_' + anonymousCount++;
+        }
 
-		if (!reqs || storedModule.executed === true) return false;
+        // Track module status and properties.
+        modules[module] = {
+            'resolved': false,
+            'value': callback,
+            'dependencies': deps,
+            'built': (!deps.length && !callback) ? true : false,
+            'shimDepsWaiting': isShim
+        };
+        allDeps[module] = allDeps[module] || [];
 
-		if (typeof storedModule.value === 'function') {
-			storedModule.value = storedModule.value.apply(storedModule.value, reqs);
-		} else storedModule.value = undefined;
+        // Loop through dependencies to register them
+        i = deps.length;
+        while (i--) {
+            if (!deps[i]) continue;
+            // Account for type prefixing like `json!`.
+            deps[i] = attachType(deps[i]);
+            allDeps[deps[i]] = allDeps[deps[i]] || [];
+            if (shim[deps[i]]) {
+                // Shimmed dependencies, so load handle dependencies first.
+                handleModule(deps[i], shim[deps[i]].deps, depsLoaded(deps[i], executor(deps[i])), true);
+            } else {
+                // Attempt to load the dependency.
+                loadModule(deps[i], executor(deps[i]));
+            }
+            // Register a new dependency.
+            if (allDeps[deps[i]].indexOf(module) === -1) {
+                allDeps[deps[i]].push(module);
+            }
+        }
 
-		storedModule.executed = true;
-		
-		while (i--) {
-			this.execute(this._reqs[module][i]);
-		}
-	};
-	ModulesList.prototype.include = function(module, executor) {
-		var script = document.createElement('script'),
-			firstScript = document.getElementsByTagName('script')[0],
-			loaded,
-			Modules = this;
+        // Maybe the module _has_ all dependencies, so try it.
+        if (typeof callback === 'function') attemptExecute(module);
 
-		//for not duplicating script tags
-		if (typeof this._modules[module] === 'object') return true;
-		this._modules[module] = {};
+        // Closure functions to pass around as an executor for this module.
+        function executor(module) {
+            return function() {
+                modules[module].shimDepsWaiting = false;
+                attemptExecute(module);
+            };
+        }
+        function depsLoaded(req, callback) {
+            return function() {
+                loadModule(req, callback);
+            };
+        }
+    }
+    // ## Get Dependencies
+    // Return an array of module dependencies, or false if any are not yet
+    // resolved.
+    function getDependencies(module) {
+        var results = [];
+        var deps = modules[module].dependencies || [];
+        var i = deps.length;
 
-		script.type = 'text/javascript';
-		script.async = true;
-		script.onreadystatechange = script.onload = function(e) {
-			var state = this.readyState;
-			if (!loaded && (!state || state === 'complete' || state === 'loaded')) {
-				this.onreadystatechange = null;
-				loaded = 1;
-				executor();
-			}
-		};
-		script.src = module;
-		firstScript.parentNode.insertBefore(script, firstScript);
-	};
+        // **Loop through each dependency.**
+        while (i--) {
+            if (deps[i] in modules && modules[deps[i]].resolved !== true) {
+                // Module not yet registered or not yet resolved.
+                return false;
+            } else if (deps[i] in modules) {
+                // Module is resolved, so add the return value to the buffer.
+                results.unshift(modules[deps[i]].value);
+            }
+        }
+        return results;
+    }
 
-/**
- * Extend Global scope with:
- */
+    // ## Attempt Execute
+    // If a module's dependencies are resolved, execute the module
+    // and capture the value.  Next, attempt the same on 'parent' dependencies,
+    // because that might be the last dependency they needed.
+    function attemptExecute(module, forced) {
+        var results;
+        var i = allDeps[module].length;
+        var storedModule = modules[module];
+        var moduleDeps = getDependencies(module); // check dependencies
 
-/**
- * require
- * 
- * AMD-esque require function
- * 
- * @param  {Array||String}  reqs
- *         A list of strings for dependencies
- * @param  {Function}  callback  Optional
- *         function to perform after dependencies are loaded
- * @return {undefined}
- */
-	window.require = function(reqs, callback) {
-		Modules.add(undefined, reqs, callback);
-	};
+        if (!moduleDeps || storedModule.resolved === true) {
+            // Not all dependencies are resolved, or this was resolved already. 
+            return false;
+        }
 
-/**
- * define
- * 
- * AMD-esque define function
- * 
- * @param  {String} id
- *         filename of included file
- * @param  {Array} reqs
- *         list of module dependencies
- * @param  {Function} module
- *         callback that returns the module object
- * @return {undefined}
- */
-	window.define = function() {
-		Modules.add.apply(Modules, arguments);
-	};
+        if (typeof storedModule.value === 'function') {
+            // Execute function modules with dependencies as arguments.
+            storedModule.value = storedModule.value.apply(storedModule.value, moduleDeps);
+            storedModule.built = true;
+        } else if (shim.hasOwnProperty(module) && shim[module].exports) {
+            // Module has a global export shim, so grab the export value.
+            storedModule.value = window[shim[module].exports];
+            storedModule.built = true;
+        } else if (forced) {
+            // Force resolution of the module, from the public `.execute()` 
+            storedModule.built = true;
+        }
 
-/**
- * domReady requirement definition
- * add 'domReady' requirement to only fire after DOM is ready
- */
-	Modules.add('domReady');
-	var domReady = function() {
-		Modules.execute('domReady');
-	};	
-	if (document.addEventListener) {
-		document.addEventListener('DOMContentLoaded', domReady, false);
-		window.addEventListener('load', domReady, false);
-	} else {
-		document.attachEvent('onreadystatechange', domReady);
-		window.attachEvent('onload', domReady);
-	}
+        if (!storedModule.shimDepsWaiting) {
+            storedModule.resolved = true;
+        }
+        
+        while (i--) {
+            attemptExecute(allDeps[module][i]);
+        }
+    }
+    // ## Load Module
+    // Build a script tag or execute an XHR call for a module, unless the tag
+    // has already been built.
+    function loadModule(module, executor) {
+        // For not duplicating script tags, register the tag as built.
+        if (typeof modules[module] !== 'object') {
+            modules[module] = {built:true};
+        } else if (modules[module].built) {
+            // Already built, so return.
+            return true;
+        } else {
+            modules[module].built = true;
+        }
+
+        if (module.type === 'script') {
+            // Standard types are script tags, injected into the DOM.
+            var script = document.createElement('script');
+            var firstScript = document.getElementsByTagName('script')[0];
+            var loaded;
+
+            // Inject the script tag.
+            script.type = 'text/javascript';
+            script.async = true;
+            script.onreadystatechange = script.onload = function(e) {
+                var state = this.readyState;
+                if (!loaded && (!state || state === 'complete' || state === 'loaded')) {
+                    this.onreadystatechange = null;
+                    loaded = 1;
+                    // Script loaded, so attempt to resolve the module..
+                    executor();
+                }
+            };
+            script.src = module;
+            firstScript.parentNode.insertBefore(script, firstScript);
+        } else if (module.type === 'json') {
+            // `json!` file prefixes are JSON GET requests using XHR.
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', module, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState != 4 || xhr.status != 200) return;
+                // Parse response JSON.
+                if (xhr.responseText) modules[module].value = JSON.parse(xhr.responseText);
+                else modules[module].value = {};
+                // JSON loaded, so attempt to resolve the module.
+                executor();
+            };
+            // Attach any `onXhr` configs to the XHR request.
+            if (config.onXhr) config.onXhr.call(Modules, xhr, module.toString());
+            xhr.send();
+        }
+    }
+
+    // ## Attach Type
+    // Create a complex String object to attach a type property, based on a
+    // prefix (eg, 'json!/foo/bar')
+    function attachType(module) {
+        if (!module || !module.split) return module;
+        var modgroup = module.split('!').reverse();
+        if (paths.hasOwnProperty(modgroup[0])) {
+            modgroup[0] = paths[modgroup[0]];
+        }
+        module = new String(modgroup[0]);
+        module.type = modgroup[1] || 'script';
+        return module;
+    }
+
+    // Configure the AMD Loader
+    // Use this for a shim config similar to _RequireJS_
+    function configModuleHandler(cfg) {
+        paths = cfg.paths || paths;
+        shim = cfg.shim;
+        config = cfg;
+
+        var i, shimpath;
+
+        for (shimpath in shim) {
+            // Handle registering shim configs with path nicknames
+            if (!shim.hasOwnProperty(shimpath)) continue;
+            if (paths.hasOwnProperty(shimpath)) {
+                shim[paths[shimpath]] = shim[shimpath];
+                delete shim[shimpath];
+                shimpath = paths[shimpath];
+            }
+            i = shim[shimpath].deps && shim[shimpath].deps.length ? shim[shimpath].deps.length : 0;
+            while (i--) {
+                if (paths.hasOwnProperty(shim[shimpath].deps[i])) {
+                    shim[shimpath].deps[i] = paths[shim[shimpath].deps[i]];
+                }
+            }
+        }
+    }
+
+// ## domReady` dependency definition
+// add `domReady` dependency to wait until the DOM is ready to manipulate
+    Modules.define('domReady');
+    var domReady = function() {
+        Modules.execute('domReady');
+    };
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', domReady, false);
+        window.addEventListener('load', domReady, false);
+    } else {
+        document.attachEvent('onreadystatechange', domReady);
+        window.attachEvent('onload', domReady);
+    }
 
 }(window, document));
